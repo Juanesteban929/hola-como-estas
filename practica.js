39aeeb8ead7ac035799175855f380ba02aa49732
Juanesteban929 <81245169+Juanesteban929@users.noreply.github.com>
2021-06-09 09:43:43 -0500

    suma

---
+++ practica.js
@@ -0,0 +1,17 @@
+//mi primera practica
+//este es un comentario
+/* y este es un comertario multilinea */
+//autor juan esteban moncayo
+const nombre = 'Juan esteban';
+var tipo_doc = '3016472943';
+let edad = '15';
+console.log ("Buenas tardes señorito "+nombre+"tu numero de celular es: "+tipo_doc+"con una edad de: "+edad)
+
+let suma = 0
+suma = suma + 20;
+console.log(suma);
+suma = suma + 6;
+console.log(suma);
+suma = suma + 6 ;
+console.log(suma);
+
