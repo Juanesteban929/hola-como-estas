fc865b15d0d49956f97893a0c5e16c008c77ddd2
Juanesteban929 <81245169+Juanesteban929@users.noreply.github.com>
2021-09-02 17:20:38 -0500

    6 tablas

--- Censorecidente.session.sql
+++ Censorecidente.session.sql
@@ -3,7 +3,9 @@ CREATE TABLE parqueadero
     id_parqueadero SERIAL,
     num_parqueadero VARCHAR,
     matricula_parqueadero BIGINT,
-    coef_parqueadero FLOAT
+    coef_parqueadero FLOAT,
+    CONSTRAINT pk_parqueadero PRIMARY KEY (id_parqueadero),
+    CONSTRAINT fk_residente FOREIGN KEY (id_residente) REFERENCES residente(id_residente)
 )
 
  CREATE TABLE mascota
@@ -12,24 +14,72 @@ CREATE TABLE parqueadero
      raza VARCHAR,
      nombre_mascota VARCHAR,
      especie_mascota VARCHAR,
-     id_recidente INTEGER
+     id_recidente INTEGER,
+      CONSTRAINT pk_mascota PRIMARY KEY (id_mascota)
+    CONSTRAINT fk_residente FOREIGN KEY (id_residente) REFERENCES residente(id_residente)
  )
 
  CREATE TABLE residente
  (
      id_residente SERIAL,
-     nombre_residente VARCHAR
+     nombre_residente VARCHAR,
+     apellido_residente VARCHAR,
     edad INTEGER,
     religion VARCHAR,
     id_representante INTEGER,
     lugar_trabajo VARCHAR,
-    eps ,
-    tel_residente,
-    vacuna,
-    condicion_salud,
-    correo_residente,
-    estado_civil
+    eps VARCHAR,
+    tel_residente VARCHAR,
+    vacuna VARCHAR,
+    condicion_salud VARCHAR,
+    correo_residente VARCHAR,
+    estado_civil VARCHAR,
+    CONSTRAINT pk_residente PRIMARY KEY (id_residente)
+
  )
+
+
+CREATE TABLE apartamento
+(
+    id_apto serial,
+    matricula_apto VARCHAR,
+    num_apto VARCHAR,
+    bloque VARCHAR,
+    coef_apto FLOAT,
+    CONSTRAINT pk_apartamento PRIMARY KEY (id_apto),
+    CONSTRAINT fk_residente FOREIGN KEY (id_residente) REFERENCES residente(id_residente)
+)
+
+CREATE TABLE vehiculo
+(
+    id_vehiculo SERIAL,
+    tipo_de_vehiculo VARCHAR,
+    placa VARCHAR,
+    modelo VARCHAR,
+    marca VARCHAR,
+    id_residente INTEGER,
+    CONSTRAINT pk_vehiculo PRIMARY KEY (id_vehiculo),
+CONSTRAINT fk_residente FOREIGN KEY (id_residente) REFERENCES residente(id_residente)
+)
+
+CREATE TABLE propietario
+(
+    id_propietario SERIAL,
+    tipo_doc_propietario VARCHAR,
+    num_identificacion VARCHAR,
+    tel_propietario BIGINT,
+    nom_propietario VARCHAR,
+    apell_propietario VARCHAR,
+    ocupacion VARCHAR,
+    edad SMALLINT,
+    vacuna BOOLEAN,
+    razon_social VARCHAR,
+    corrreo_propietario VARCHAR,
+    CONSTRAINT pk_propietario PRIMARY KEY (id_propietario),
+)
+
+
+
 
 
 --inserts (create)
